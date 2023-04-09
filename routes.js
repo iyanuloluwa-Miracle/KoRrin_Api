b34b0e4cb83f42ec6b207c430462510c1ff7339e
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-04-09 06:57:31 -0700

    debugging

--- routes.js
+++ routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const sendInvite = require('./userInvite.controller');
-const newPageController = require('./newPage.controller');
+
 
 // Define the route for sending an invite
 router.post('/sendInvite', sendInvite.sendInvite);
 
-// Define the route for the new page
-router.get('/newPage', newPageController.newPage);
+
 
 module.exports = router;
--- userInvite.controller.js
+++ userInvite.controller.js
@@ -3,17 +3,15 @@ exports.sendInvite = async (req, res) => {
   const price = req.body.price;
   
   // Check if a fan was selected
-  if (!fan) {
-    return res.status(400).json({ error: 'Please select at least one fan' });
+  if (!fan || fan.length === 0) {
+    return res.status(400).json({ error: 'Please select at least one fan.' });
   }
   
   // Check if the price is valid
   if (isNaN(price) || price <= 0) {
-    return res.status(400).json({ error: 'Please set a valid price' });
+    return res.status(400).json({ error: 'Please set a valid price.' });
   }
 
-  
-  
   // Redirect the user to a new page
-  res.send('Hey Damilola you just sent an Invite to your fans');
-}
+  res.send('Hey Damilola, you just sent an invite to your fans!');
+};
